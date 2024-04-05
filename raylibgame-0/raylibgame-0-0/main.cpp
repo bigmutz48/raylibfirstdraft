04a4eac56a376a9bebdd240cd2134f0a099cdacf
U-ACADEMICWEAPONB\wesse <wesselsimon00@gmail.com>
2024-04-04 17:27:45 -0700

    trying to incorporate cmake

--- raylibgame-0/raylibgame-0-0/main.cpp
+++ raylibgame-0/raylibgame-0-0/main.cpp
@@ -33,22 +33,22 @@ class introduction{
       if  (IsKeyDown(KEY_Y)) {
         break;
       } else if (IsKeyDown(KEY_N)) {
-        ClearBackgroun(BLACK);
+        ClearBackground(BLACK);
         DrawText("Yeah ... I get it .... \n", 10, 10, 20);
         std::this_thread::sleep_for(std::chrono::seconds(2));
       }
       
 
         ClearBackgroun(BLACK);
-      DrawText("You wake up on the ground \n" , 10, 10, 20, WHITE);
+      DrawText(" You stand up. \n" , 10, 10, 20, WHITE);
       std::this_thread::sleep_for(std::chrono::seconds(2));
 
         ClearBackgroun(BLACK);
-      DrawText("You wake up on the ground \n" , 10, 10, 20, WHITE);
+      DrawText("Your hear your joints popping. \n" , 10, 10, 20, WHITE);
       std::this_thread::sleep_for(std::chrono::seconds(2));
 
         ClearBackgroun(BLACK);
-      DrawText("You wake up on the ground \n" , 10, 10, 20, WHITE);
+      DrawText("Your really need to start taking better care of yourself. \n" , 10, 10, 20, WHITE);
       std::this_thread::sleep_for(std::chrono::seconds(2));
 
         ClearBackgroun(BLACK);
@@ -66,9 +66,12 @@ int main () {
   InitWindow(800, 600 "Welcome to 0.00 !");
 
   while (!WindowShouldClose()) { // main game functions should be called in here
+    introduction callable_intro;
+    callable_intro.waking_up();
+
 
     
-
+    
 
 
 
